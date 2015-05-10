2f73456e06d4f56996eef20a6b0ca536aa492667
matt <matt@pltech.co.nz>
2015-05-11 11:38:31 +1200

    Make sure the numerical casting is done in plots, not outside

--- plot4.R
+++ plot4.R
@@ -7,9 +7,9 @@ plot(df$DateTime,as.double(as.character(df$Voltage)),type='l',ylab="Voltage",xla
 
 plotColors <- c("black", "red", "blue")
 lineLabels <- c("Sub_metering_1","Sub_metering_2","Sub_metering_3")
-plot(df$DateTime,as.double(df$Sub_metering_1),type='l',ylab="Energy sub metering",xlab="",col=plotColors[1],lty=1)
-lines(df$DateTime,as.double(df$Sub_metering_2),type='l',ylab="Energy sub metering",xlab="",col=plotColors[2],lty=1)
-lines(df$DateTime,as.double(df$Sub_metering_3),type='l',ylab="Energy sub metering",xlab="",col=plotColors[3],lty=1)
+plot(df$DateTime,as.double(as.character(df$Sub_metering_1)),type='l',ylab="Energy sub metering",xlab="",col=plotColors[1],lty=1)
+lines(df$DateTime,as.double(as.character(df$Sub_metering_2)),type='l',ylab="Energy sub metering",xlab="",col=plotColors[2],lty=1)
+lines(df$DateTime,as.double(as.character(df$Sub_metering_3)),type='l',ylab="Energy sub metering",xlab="",col=plotColors[3],lty=1)
 legend("topright", lineLabels, cex=0.8, col=plotColors, lty=1, lwd=2, bty="l")
 
 plot(df$DateTime,as.double(as.character(df$Global_reactive_power)),type='l',ylab="Global_reactive_power",xlab="datetime")
