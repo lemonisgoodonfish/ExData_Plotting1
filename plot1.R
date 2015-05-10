609eb3ae8e5f5c5d27b1dee3bec6c119667c7cb1
matt <matt@pltech.co.nz>
2015-05-11 11:01:51 +1200

    adding solutions and outputs for plot 1-3

---
+++ plot1.R
@@ -0,0 +1,4 @@
+# load processData first and run df = go() before running this file an other plot files
+png(filename="plot1.png",width=480,height=480)
+hist(as.numeric(df$Global_active_power),main="Global active power",xlab="Global Active Power (kilowatts)",col = c("red"))
+dev.off()
---
+++ plot2.R
@@ -0,0 +1,3 @@
+png(filename="plot2.png",width=480,height=480)
+plot(df$DateTime,as.numeric(df$Global_active_power)/1000,type='l',ylab="Global Active Power (Kilowatts)",xlab="")
+dev.off()
